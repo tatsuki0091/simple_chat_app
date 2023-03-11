960d5ad533d5a7f9a8d1aa0b4b952048d252ce8b
tatsuki yamatani <tatsuki.yamatani@bittreo.com>
2023-03-11 11:42:30 -0800

    Finished creating login UI form

---
+++ client/src/app/auth/login/Login.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+        <div className="rounded-lg bg-white p-12 shadow-lg">
+          <h2 className="mb-4 text-2xl font-bold">Login</h2>
+          <form>
+            <div className="mb-4">
+              <label
+                className="mb-2 block font-bold text-gray-700"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 focus:outline-none"
+                id="username"
+                type="text"
+                placeholder="Username"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="mb-2 block font-bold text-gray-700"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-gray-700 focus:outline-none"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <button
+              className="focus:shadow-outline w-full rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
+              type="button"
+            >
+              Sign In
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ client/src/app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from "./Login";
+
+const page = () => {
+  return <Login />;
+};
+
+export default page;
