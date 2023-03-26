25e8cf296069739e2b193612ddb88a80c68e7cf0
Tatsuki Yamatani <yao099991@gmail.com>
2023-03-25 22:39:17 -0700

    Added mongo config

---
+++ server/mongo-init.js
@@ -0,0 +1,10 @@
+db.createUser({
+  user: "<user for database which shall be created>",
+  pwd: "<password of user>",
+  roles: [
+    {
+      role: "readWrite",
+      db: "<database to create>",
+    },
+  ],
+});
