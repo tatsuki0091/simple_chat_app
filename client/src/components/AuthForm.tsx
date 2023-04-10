ff4413c9fc6f067a0e13111a05ed4301065ba542
Tatsuki Yamatani <yao099991@gmail.com>
2023-04-10 00:21:44 -0700

    Added username to register user

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -1,21 +1,29 @@
 "use client";
 import React from "react";
 import { useInput } from "../hooks/useInput";
+import { useForm } from "../hooks/useForm";
+import { Submit } from "../interfaces/auth";
 
-interface Submit {
-  submit: string;
-  label: string;
-}
-
-const AuthForm = ({ submit, label }: Submit) => {
+const AuthForm = ({ submit, label, url }: Submit) => {
   const [email, handleEmail, resetEmail] = useInput("");
   const [password, handlePassword, resetPassword] = useInput("");
+  const userInfo = { email: email, password: password };
+  console.log("test");
+  const sendRequest = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("sendRequest");
+    const apiResponse = await useForm({ values: userInfo, url: url });
+    console.log(apiResponse);
+    resetEmail();
+    resetPassword();
+  };
+
   return (
     <>
       <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
         <div className="rounded-lg bg-white p-12 shadow-lg">
           <h2 className="mb-4 text-2xl font-bold">{label}</h2>
-          <form>
+          <form onSubmit={sendRequest}>
             <div className="mb-4">
               <label
                 className="mb-2 block font-bold text-gray-700"
@@ -50,7 +58,7 @@ const AuthForm = ({ submit, label }: Submit) => {
             </div>
             <button
               className="focus:shadow-outline w-full rounded bg-blue-500 py-2 px-4 font-bold text-white hover:bg-blue-700 focus:outline-none"
-              type="button"
+              type="submit"
             >
               {submit}
             </button>
---
+++ client/src/interfaces/auth.ts
@@ -0,0 +1,5 @@
+export interface Submit {
+  submit: string;
+  label: string;
+  url: string;
+}
